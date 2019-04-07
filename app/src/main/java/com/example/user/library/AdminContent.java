edcf06de270db23998829e6a4889f1c216c476e6
Anastasya34 <Anastasya.Alecs.Ko@yandex.ru>
2019-04-07 14:35:54 +0300

    Add empty admin interface

---
+++ app/src/main/java/com/example/user/library/AdminContent.java
@@ -0,0 +1,7 @@
+package com.example.user.library;
+
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+public class AdminMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+}
